3a9f9b327117db031c1d279c64f5d914a4d53a9f
abpandanguyen <abpandanguyen@gmail.com>
2022-07-22 17:22:52 -0700

    connect to database

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+
+// Loading stuff in the .env module!
+require('dotenv').config();
+// Connect to database
+require('./config/database');
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
