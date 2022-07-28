b04cdd05375c2de85e49a0ebfbca3cbfeb123e5a
abpandanguyen <abpandanguyen@gmail.com>
2022-07-27 19:39:41 -0700

    rating change

--- models/meme.js
+++ models/meme.js
@@ -10,7 +10,7 @@ const commentSchema = new Schema({
     rating: {
         type: Number,
         min: 1,
-        max: 10,
+        max: 5,
         default: 5
     },
     user: {type: Schema.Types.ObjectId, ref: 'User'},
